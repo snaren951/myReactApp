5b4f15dfc3f3c48d54456d4f24c3b85218c0ef1a
Naren <snaren951@gmail.com>
2025-11-23 01:04:11 -0500

    playing with react elements, components, and jsx

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
  
- 
+ /*
  const heading = React.createElement("div",{id: "parent"},
                     React.createElement("div",{id: "child"},
                         [React.createElement("h1",{id: "heading1"}, "This heading is done by h1 tag"),
                          React.createElement("h2",{id: "heading2"}, "This heading is done by h2 tag")]));
+*/
 
+const elem1 = <h2>React JSX Element</h2>;
+const elem2 =<span> {elem1} </span>;
 
+const Title = ()=> (<div className="head" tabIndex="5">
+    <h1>Title JSX Element</h1>
+    {elem2}
+    </div>);
+
+const Jsxheading = ()=>  (
+    <div>
+         {Title()}
+
+        <h1 id="parent">Namaste React by JSX</h1>
+
+    </div>
+    
+       
+   
+    );
 
  const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(heading);
+ root.render(<Jsxheading/>);
