86761dcb7d624729f9c74e3919f5ab9367aa17c0
Naren <snaren951@gmail.com>
2025-12-03 00:59:43 -0500

    added componentdidmount and made an api call to github to fetch my userinfo

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,37 +7,69 @@ class UserClass extends React.Component{
         console.log("user class constructor");
         super(props);
         this.state = {
-            count : 0
+            info: {
+                name:null,
+            avatar_url:null,
+            location:null
+
+            },
+            
+            counter: {
+                count:0
+            }
+            
+        };
+           
+           console.log(this.state);
+               
+           
         }
+        
        
-    }
-    componentDidMount(){
+    
+    async componentDidMount(){
 
         console.log("user class ComponentDidMount");
+        const userData = await fetch("https://api.github.com/users/snaren951");
+        const data = await userData.json();
+        console.log(data);
+        this.setState({
+            info: data
+        });
+        
+    }
+    componentDidUpdate(){
+        console.log("This will be called every time the component is updated");
+    }
+
+    componentWillUnmount(){
+
+        console.log(" It will be called when we leave the page/when the component is removed from the DOM");
     }
 
     render(){
         console.log("userclass render");
-        const {name, contact} = this.props;
+        const {name, location, avatar_url} = this.state.info;
+        const {count} = this.state.counter;
+      
+        
 
 
         return (
             <div>
-                
+                <img src={avatar_url}></img>
                 <h2>Name: {name},</h2>
+                <h2>Location: {location}</h2>
+                <h3>Count: {count}</h3>
+                <button onClick={
+                    ()=>{
+                       
+                        this.setState({
+                            counter: {count: count+1}
+                        });
 
-                
-                <h3> Contact: {contact}</h3>
-                <h4>Count: {this.state.count}</h4>
-                <button onClick={()=>{
-                    this.setState({
-                        count: this.state.count+1
-                    })
-                }}>Click Me</button>
-                
-
-                
-                
+                    }
+                }>Click Me</button>
                 
             </div>
             
